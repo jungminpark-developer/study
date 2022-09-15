8c83a29e7d28774f69cbb4cf274c12fd0020e87e
jpark.dev <jpark.dev@daum.net>
2022-09-15 19:34:30 +0900

    chore: update Setter and Getter

--- src/GitHub/Car.java
+++ src/GitHub/Car.java
@@ -6,22 +6,36 @@ public class Car {
 	private String modelName;
 	private int maxSpeed;
 	
-	//Setter-자동차 정보 매개변수로 입력하기
+	//getter and setter	
+	public int getModelNo() {
+		return modelNo;
+	}
+
+	public void setModelNo(int modelNo) {
+		this.modelNo = modelNo;
+	}
+
+	public String getModelName() {
+		return modelName;
+	}
+
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+
+	public void setMaxSpeed(int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+
+	//자동차 정보 매개변수로 입력하기
 	protected void setCarInfo(int modelNo, String modelName, int maxSpeed) {
 		this.modelNo = modelNo;
 		this.modelName = modelName;
 		this.maxSpeed = maxSpeed;
-	}
-	
-	//Getter
-	protected int getModelNo() {
-		return modelNo;
-	}
-	protected String getModelName() {
-		return modelName;
-	}
-	protected int getMaxSpeed() {
-		return maxSpeed;
 	}
 	
 	//Method-출력 전용
