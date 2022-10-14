e18f5610c0193cb94487a697f1b9aacc78dce356
jpark.dev <jpark.dev@daum.net>
2022-10-14 15:28:06 +0900

    test: Studying CodingTest

---
+++ src/codingtest/ArrayStringLength.java
@@ -0,0 +1,31 @@
+package codingtest;
+
+public class ArrayStringLength {
+	
+	public int[] solution(String[] strlist) {
+		int[] answer = {};
+
+		for(int i=0; i<strlist.length; i++){
+			answer[i] = strlist[i].length();
+		}
+		
+		for(int i=0; i<answer.length; i++){
+			System.out.println(answer[i]);
+		}
+
+		return answer;
+	}
+	
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayStringLength test = new ArrayStringLength();
+		String[] strlist = {"We", "are", "the", "world!"};
+		
+		System.out.println(strlist[3].length());
+		System.out.println(test.solution(strlist));
+		
+	}
+
+}
---
+++ src/codingtest/DividePizza1.java
@@ -0,0 +1,25 @@
+package codingtest;
+
+public class DividePizza1 {
+
+		public int solution(int n) {
+			int answer = 1;
+
+			while(7 * answer < n){
+				answer++;
+			}
+
+			return answer;
+	    }
+	
+	public static void main(String[] args) {
+		//피자 나눠 먹기 (1)-프로그래머스 0단계
+		DividePizza1 test = new DividePizza1();
+		
+		System.out.println("2명이 최소 한조각씩 먹기 위해서 최소 "+test.solution(2)+"판이 필요합니다.");
+		System.out.println("11명이 최소 한조각씩 먹기 위해서 최소 "+test.solution(11)+"판이 필요합니다.");
+		System.out.println("21명이 최소 한조각씩 먹기 위해서 최소 "+test.solution(21)+"판이 필요합니다.");
+		System.out.println("22명이 최소 한조각씩 먹기 위해서 최소 "+test.solution(22)+"판이 필요합니다.");
+		
+	}
+}
