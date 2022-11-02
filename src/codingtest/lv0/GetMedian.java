4efa40833114b790b38690896ea2c47030d705b8
jpark.dev <jpark.dev@daum.net>
2022-11-02 17:39:05 +0900

    test: Studying CodingTest

--- src/codingtest/lv0/GetMedian.java
+++ src/codingtest/lv0/GetMedian.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class GetMedian {
 	
 	public int solution(int[] array) {
+		//중앙값 구하기-프로그래머스 0단계
 		int answer = 0;
 		Arrays.sort(array);  
 		answer = array.length/2;  
