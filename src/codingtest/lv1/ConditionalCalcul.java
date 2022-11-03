fcdfcf639988800076c90738b70451c5b8d96fe2
jpark.dev <jpark.dev@daum.net>
2022-11-03 17:40:56 +0900

    test: Studying CodingTest

---
+++ src/codingtest/lv1/ConditionalCalcul.java
@@ -0,0 +1,28 @@
+package codingtest.lv1;
+
+public class ConditionalCalcul {
+
+	public long solution(int price, int money, int count) {
+		//부족한 금액 계산하기-프로그래머스 1단계
+		long answer = 0;
+		
+		for(int i=1; i<=count; i++) {
+			answer += price * i;
+		}
+		
+		if(answer <= money) {
+			return 0;
+		}else {
+			return answer - money;
+		}
+		
+//		return answer;
+	}
+	
+	public static void main(String[] args) {
+		//실행
+		ConditionalCalcul test = new ConditionalCalcul();
+		System.out.println(test.solution(3, 20, 4));
+		
+	}
+}
---
+++ src/codingtest/lv1/GetMiddleCharacter.java
@@ -0,0 +1,22 @@
+package codingtest.lv1;
+
+public class GetMiddleCharacter {
+
+	public String solution(String s) {
+		//가운데 글자 가져오기-프로그래머스 1단계
+		String answer = "";
+		
+		if(s.length() % 2 == 1) { //글자수가 홀수라면
+			answer += s.charAt(s.length()/2);
+		}else {
+			answer += s.substring(s.length()/2-1, s.length()/2+1);
+		}
+		
+		return answer;
+	}
+	public static void main(String[] args) {
+		//실행
+		GetMiddleCharacter test = new GetMiddleCharacter();
+		System.out.println(test.solution("abcd"));
+	}
+}
