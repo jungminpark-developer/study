4c05f542acfccd7388a8707e1fc3d61286e94a26
jpark.dev <jpark.dev@daum.net>
2022-11-07 14:24:11 +0900

    test: Studying CodingTest

---
+++ src/codingtest/lv0/GetOddNumber.java
@@ -0,0 +1,24 @@
+package codingtest.lv0;
+
+public class GetOddNumber {
+
+	public int[] solution(int n) {
+		int count = 0;
+		if(n % 2 == 0) {
+			count = n/2;
+			n--;
+		}else{
+			count = n/2 + 1;
+		}
+		
+		int[] answer = new int[count];
+		int j = 1;
+		
+		for(int i=0; i<answer.length; i++) {
+			answer[i] = j;
+			j += 2;
+		}
+		
+		return answer;
+	}
+}
