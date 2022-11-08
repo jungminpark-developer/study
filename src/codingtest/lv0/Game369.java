e9f7ac464a0dc7debfef29d70f257380ddfbb8b6
jpark.dev <jpark.dev@daum.net>
2022-11-08 16:08:31 +0900

    test: Studying CodingTest

---
+++ src/codingtest/lv0/Game369.java
@@ -0,0 +1,20 @@
+package codingtest.lv0;
+
+public class Game369 {
+	
+	public int solution(int order) {
+		//369게임-프로그래머스 0단계
+		int answer = 0;
+		String number = String.valueOf(order);
+
+		char[] array = number.toCharArray();
+
+		for(int i=0; i<array.length; i++){
+			if(array[i] == '3' || array[i] == '6' || array[i] == '9') {
+				answer++;
+			}
+		}
+
+		return answer;
+	}
+}
