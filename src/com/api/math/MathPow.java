3cb579a618e16798b7eb63587c0463e86025f6ff
jungminpark <jpark.dev@daum.net>
2022-11-13 16:28:46 +0900

    feat: Studying API

---
+++ src/com/api/math/MathPow.java
@@ -0,0 +1,12 @@
+package com.api.math;
+
+public class MathPow {
+
+	public static void main(String[] args) {
+		// 221113 거듭 제곱 구하기
+		// 리턴값은 double
+		double result = Math.pow(2, 3);
+		System.out.println("2의 세제곱은: " + result);
+
+	}
+}
